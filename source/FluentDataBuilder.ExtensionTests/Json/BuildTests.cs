5fb5816cc74c73afaeb964f38616378be258ecf2
Lars Krämer <lars.kraemer@live.de>
2023-08-03 22:24:55 +0200

    FDB-6: removed invalid tests for later usage

--- source/FluentDataBuilder.ExtensionTests/Json/BuildTests.cs
+++ source/FluentDataBuilder.ExtensionTests/Json/BuildTests.cs
@@ -71,21 +71,21 @@ public class BuildTests
         result.Should().Be("{\"name\":\"this is a test\",\"number\":123}");
     }
 
-    [TestMethod]
-    public void Build_WithStringArray_Returns()
-    {
-        IDataBuilder builder = new DataBuilder();
-        
-        builder.Add("name", "this is a test");
-        builder.Add("array", new List<string> {"this", "is", "a", "test"}.ToArray());
-
-        JsonDocument jsonResult = builder.Build();
-
-        string result = jsonResult.RootElement.GetRawText();
-
-        result.Should().NotBeNullOrEmpty();
-        result.Should().Be("{\"name\":\"this is a test\",\"number\":123}");
-    }
+    // [TestMethod]
+    // public void Build_WithStringArray_Returns()
+    // {
+    //     IDataBuilder builder = new DataBuilder();
+    //     
+    //     builder.Add("name", "this is a test");
+    //     builder.Add("array", new List<string> {"this", "is", "a", "test"}.ToArray());
+    //
+    //     JsonDocument jsonResult = builder.Build();
+    //
+    //     string result = jsonResult.RootElement.GetRawText();
+    //
+    //     result.Should().NotBeNullOrEmpty();
+    //     result.Should().Be("{\"name\":\"this is a test\",\"number\":123}");
+    // }
 
     [TestMethod]
     public void Build_WithMixedArray_Returns()
