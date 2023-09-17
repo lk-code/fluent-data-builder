e9c8d7290a717f6da79c36cbfcb2372c326c1102
Lars Krämer <lars.kraemer@live.de>
2023-09-17 14:03:08 +0200

    FDB-38: updated FDB Core to removed warnings and enable nullable values

--- source/FluentDataBuilder/IDataBuilder.cs
+++ source/FluentDataBuilder/IDataBuilder.cs
@@ -11,7 +11,7 @@ public interface IDataBuilder
     /// <param name="name">The property name for the entry.</param>
     /// <param name="value">The value to add.</param>
     /// <returns>The IDataBuilder object.</returns>
-    IDataBuilder Add(string name, object value);
+    IDataBuilder Add(string name, object? value);
 
     /// <summary>
     /// Adds a complex value.
@@ -33,12 +33,12 @@ public interface IDataBuilder
     /// Retrieves all added properties as a dictionary.
     /// </summary>
     /// <returns>A dictionary containing the properties.</returns>
-    Dictionary<string, object> GetProperties();
+    Dictionary<string, object?> GetProperties();
 
     /// <summary>
     /// Gets or sets the value associated with the specified key.
     /// </summary>
     /// <param name="key">The key of the element to get or set.</param>
     /// <returns>The value associated with the specified key.</returns>
-    object this[string key] { get; set; }
+    object? this[string key] { get; set; }
 }
